de709b2ae3d3ad7c7a5c0f4f3dacfb9af208ca0a
Oceane Villeneuve <oceanevilleneuve16@gmail.com>
2024-01-15 13:47:37 +0100

    the background is changing on projects page

--- src/components/project/index.jsx
+++ src/components/project/index.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
-// import styled from 'styled-components';
-// import colors from '../../utils/style/colors';
+import styled from 'styled-components';
+import { useMediaQuery } from "react-responsive";
+
+const ContainerProjects = styled.div`
+  position: relative;
+`;
 
 
 const Projects = () => {
-  // const isMobile = useMediaQuery({ maxWidth: 767 });
+  const isMobile = useMediaQuery({ maxWidth: 767 });
   // const location = useLocation();
 
   return (
-    <div id="projectsPage">
+    <ContainerProjects
+    id="projectsPage"
+    style={{
+      minHeight: isMobile ? "70vh" : "100vh",
+    }}>
+      <p>Hello oeeo</p>
     {/* Contenu du composant */}
     {/* Parallax framer motion */}
-    </div>
+    </ContainerProjects>
   )
 };
 
